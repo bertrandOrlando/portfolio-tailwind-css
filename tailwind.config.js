6997a6b8d03b6d91116657e262f5bdb4c44347a6
Frox8 <bertrandorlando@gmail.comgit config --global user.email bertrandorlando@gmail.com>
2022-08-19 16:54:43 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        main: "#a16207",
+        dark: "#0f172a",
+        grey: "#94a3b8",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
